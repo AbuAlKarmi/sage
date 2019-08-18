6c2f50fe4cadd7d98979b6a50509a3d5df4ff9ec
Mohammad Karmi <karmi@525k.io>
2019-08-18 21:39:35 +0300

    co-authores

--- resources/views/partials/entry-meta.blade.php
+++ resources/views/partials/entry-meta.blade.php
@@ -1,15 +1,22 @@
 <div class="byline post-meta author vcard">
+  <?php $coAuthors = explode(',',coauthors_posts_links(',', ',', null, null, false)); ?>
   <div class="d-flex information-wrapper">
     <div class="d-inline-flex">
-      <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="author-image fn">
-        {!! get_avatar(get_the_author_meta('ID')) !!}
-      </a>
+      @if(count($coAuthors) > 1)
+        <span rel="author" class="author-image fn">
+          <img src="{{get_theme_file_uri('/resources/assets/images/default-image.jpg') }}" alt="">
+        </span>
+      @else
+        <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="author-image fn">
+          {!! get_avatar(get_the_author_meta('ID')) !!}
+        </a>
+      @endif
     </div>
     <div class="d-inline-flex">
       <div class="pl-2 justify-content-center">
-        <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="author-name d-block">
-          {{ get_the_author() }}
-        </a>
+        <div class="author-name has-link d-block">
+          {!! join($coAuthors, ', ') !!}
+        </div>
         @foreach( $subCategories as $category )
           <a href="{{$category['url']}}" class="btn btn-dark btn-sm btn-tag">
             {{$category['title']}}
