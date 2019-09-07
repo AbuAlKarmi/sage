82c3844299e9296c0ee9540b2701273aca3cd094
Mohammad Karmi <karmi@525k.io>
2019-09-07 12:31:20 +0300

    padding issues and font updates

--- resources/assets/scripts/routes/single.js
+++ resources/assets/scripts/routes/single.js
@@ -8,26 +8,38 @@ export default () => {
   }
 
 
-  jQuery('.background-color').each((index,element) => {
-    console.log( element );
-    $(element).tooltip();
-    element.addEventListener( 'click', () => {
-      const url = window.location;
-      const text = element.textContent;
-      window.open('http://twitter.com/share?url=' + url + '&text=' + encodeURIComponent(text), '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
+  const tweets = document.getElementsByClassName('background-color');
+  if( tweets.length ){
+    Array.from(tweets).forEach(tweet => {
+
+      tweet.addEventListener( 'click', () => {
+        const url = window.location;
+        const text = element.textContent;
+        window.open('http://twitter.com/share?url=' + url + '&text=' + encodeURIComponent(text), '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
+      });
+
     });
-  });
+  }
 
-  //
-  // console.log(document.getElementsByClassName('background-color'));
-  //
-  // Array.from(document.getElementsByClassName('background-color')).map(element => {
-  //
-  //   return new Popper(element, element.textContent, {placement: 'top'});
-  // });
-  // var popper = new Popper(document.getElementsByClassName('.background-color'), alert('hii'), {
-  //   placement: 'top'
-  // });
+  const footNote = document.getElementsByClassName('modern-footnotes-footnote__note');
+  if( footNote.length ){
+    Array.from(footNote).map(element => {
+      console.log(getOffset(element));
+       return console.log({
+         number: element.getAttribute('data-mfn'),
+         content: element.textContent,
+       });
+    });
+  }
 
 
+}
+
+
+function getOffset(el) {
+  const rect = el.getBoundingClientRect();
+  return {
+    left: rect.left + window.scrollX,
+    top: rect.top + window.scrollY
+  };
 }
