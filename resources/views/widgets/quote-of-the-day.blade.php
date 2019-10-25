92bd7650225077a373830d39d3bf76f8e041aaaa
Mohammad Karmi <karmi@525k.io>
2019-10-25 18:06:28 +0300

    fixing the following - center slider content - fixing quote of the day sharing

--- resources/views/widgets/quote-of-the-day.blade.php
+++ resources/views/widgets/quote-of-the-day.blade.php
@@ -20,37 +20,37 @@ global $post;
             </a>
 
             <?php
-            $shareText = get_the_title();
-            $shareLink = urlencode(get_the_permalink());
+            $shareText = wp_strip_all_tags($quote, true);
+            $shareLink = wp_get_shortlink();
             ?>
 
 
 
-            <div class="heateor_sss_sharing_container heateor_sss_horizontal_sharing" ss-offset="0" heateor-sss-data-href="{{$shareLink}}">
+            <div class="heateor_sss_sharing_container heateor_sss_horizontal_sharing" ss-offset="0" data-title="{{$shareText}}" heateor-sss-data-href="{!!$shareLink!!}">
               <ul class="heateor_sss_sharing_ul">
                 <li class="heateorSssSharingRound">
-                  <i style="width:18px;height:18px;border-radius:999px;" alt="Facebook" title="Facebook" class="heateorSssSharing heateorSssFacebookBackground" onclick="heateorSssPopup('https://www.facebook.com/sharer/sharer.php?u={{$shareLink}}')">
+                  <i style="width:18px;height:18px;border-radius:999px;" alt="Facebook" title="Facebook" class="heateorSssSharing heateorSssFacebookBackground" onclick="heateorSssPopup('https://www.facebook.com/sharer/sharer.php?u={!!$shareLink!!}')">
                     <ss style="display:block;border-radius:999px;" class="heateorSssSharingSvg heateorSssFacebookSvg"></ss>
                   </i>
                 </li>
                 <li class="heateorSssSharingRound">
-                  <i style="width:18px;height:18px;border-radius:999px;" alt="Twitter" title="Twitter" class="heateorSssSharing heateorSssTwitterBackground" onclick="heateorSssPopup('https://twitter.com/intent/tweet?text={{$shareText}}&url={{$shareLink}}')">
+                  <i style="width:18px;height:18px;border-radius:999px;" alt="Twitter" title="Twitter" class="heateorSssSharing heateorSssTwitterBackground" onclick="heateorSssPopup('https://twitter.com/intent/tweet?text={{$shareText}}&url={!!$shareLink!!}')">
                     <ss style="display:block;border-radius:999px;" class="heateorSssSharingSvg heateorSssTwitterSvg"></ss>
                   </i>
                 </li>
                 <li class="heateorSssSharingRound">
                   <i style="width:18px;height:18px;border-radius:999px;" alt="" title="" class="heateorSssSharing">
-                    <a href="http://web.whatsapp.com/send?text={{$shareText}} {{$shareLink}}" target="_blank">
+                    <a href="http://web.whatsapp.com/send?text={{$shareText}} {!!$shareLink!!}" target="_blank">
                       <ss style="display:block" class="heateorSssSharingSvg heateorSssWhatsappSvg"></ss>
                     </a>
                   </i>
                 </li>
                 <li class="heateorSssSharingRound">
-                  <i style="width:18px;height:18px;border-radius:999px;" alt="Google Bookmarks" title="Google Bookmarks" class="heateorSssSharing heateorSssGoogleBookmarksBackground" onclick="heateorSssPopup('https://www.google.com/bookmarks/mark?op=edit&bkmk={{$shareLink}}')">
+                  <i style="width:18px;height:18px;border-radius:999px;" alt="Google Bookmarks" title="Google Bookmarks" class="heateorSssSharing heateorSssGoogleBookmarksBackground" onclick="heateorSssPopup('https://www.google.com/bookmarks/mark?op=edit&bkmk={!!$shareLink!!}')">
                     <ss style="display:block;border-radius:999px;" class="heateorSssSharingSvg heateorSssGoogleBookmarksSvg"></ss>
                   </i>
                 </li>
-                <li class="heateorSssSharingRound"><i style="width:18px;height:18px;border-radius:999px;" alt="Telegram" title="Telegram" class="heateorSssSharing heateorSssTelegramBackground" onclick="heateorSssPopup('https://telegram.me/share/url?url={{$shareLink}}')">
+                <li class="heateorSssSharingRound"><i style="width:18px;height:18px;border-radius:999px;" alt="Telegram" title="Telegram" class="heateorSssSharing heateorSssTelegramBackground" onclick="heateorSssPopup('https://telegram.me/share/url?url={!!$shareLink!!}')">
                     <ss style="display:block;border-radius:999px;" class="heateorSssSharingSvg heateorSssTelegramSvg"></ss>
                   </i>
                 </li>
