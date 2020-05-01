48d5be987daaf7f04644c0b8b8eacd6ef360f2de
Mohammad Karmi <karmi@525k.io>
2020-05-01 16:58:57 +0300

    fix timeline issue after suggestion

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -12,38 +12,37 @@
 {{--      </style>--}}
       @endwhile
 
+      @if( isset($navigationPosts) )
+        <div class="pt-2"></div>
+        <hr>
+        <div class="single-post-navigation">
+          <div class="row d-flex flex-row">
+            <?php global $post ?>
+            @foreach($navigationPosts as $key => $post)
+              @if( isset($post) )
+                <div class="col-md-6 d-flex align-items-stretch post-nav-{{$key+1}}">
+                  <?php setup_postdata($post) ?>
+                  @include( App\cardPartial(), ['showDescription' => true, 'displayPostMeta' => false])
+                  <?php wp_reset_postdata(); ?>
+                </div>
+              @endif
+            @endforeach
+          </div>
+        </div>
+      @endif
 
       @if(app\isMobile())
         <hr>
         <?php query_posts([
-            'post_type' => 'post',
-            'post__not_in' => [get_the_ID()],
-            'paged'  => get_query_var('paged',0),
-            'posts_per_page'  => 20,
-          ]); ?>
+          'post_type' => 'post',
+          'post__not_in' => [get_the_ID()],
+          'paged'  => get_query_var('paged',0),
+          'posts_per_page'  => 20,
+        ]); ?>
         <div class="posts-loop" id="posts-infinite-scroll">
           @include('partials.loops.mobile')
         </div>
         <?php wp_reset_query(); ?>
-      @else
-        @if( isset($navigationPosts) )
-          <div class="pt-2"></div>
-          <hr>
-          <div class="single-post-navigation">
-            <div class="row d-flex flex-row">
-              <?php global $post ?>
-              @foreach($navigationPosts as $key => $post)
-                @if( isset($post) )
-                  <div class="col-md-6 d-flex align-items-stretch post-nav-{{$key+1}}">
-                    <?php setup_postdata($post) ?>
-                    @include( App\cardPartial(), ['showDescription' => true, 'displayPostMeta' => false])
-                    <?php wp_reset_postdata(); ?>
-                  </div>
-                @endif
-              @endforeach
-            </div>
-          </div>
-        @endif
       @endif
 
     </div>
